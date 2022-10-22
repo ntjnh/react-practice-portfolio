33cfefe44d607fad40a85f664546c48f2000a765
Nate J.M <nate@natejonah.com>
2022-10-22 14:10:47 +0100

    Convert App.css to SCSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
