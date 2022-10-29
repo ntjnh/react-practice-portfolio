0603577b476bf5a100a6a204d8b0e5086f6f15c2
Nate J.M <nate@natejonah.com>
2022-10-29 22:38:02 +0100

    Create Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-n.png'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          ate
+          <br />
+          web developer
+        </h1>
+
+        <h2>Front End Developer</h2>
+
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
