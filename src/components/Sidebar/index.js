76dd8280bbc6bd6e0ea7151b01ff7be5093fff6f
Nate J.M <nate@natejonah.com>
2022-10-29 13:19:58 +0100

    Set up Sidebar layout

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,82 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoN from '../../assets/images/logo-n.png'
+import LogoSubtitle from '../../assets/images/logo_sub-n.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faCodepen, faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons'
+
+const linkColour = "#ffffff";
 
 const Sidebar = () => (
-  <div className='nav-bar'>
-    <Link className='logo' to='/'>
-      <img src={LogoN} alt='N' />
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoN} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="Nate" />
     </Link>
+    <nav>
+      <NavLink
+        exact="true" 
+        activeclassName="active"
+        to="/"
+      >
+        <FontAwesomeIcon icon={faHome} color={linkColour}></FontAwesomeIcon>
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassName="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color={linkColour}></FontAwesomeIcon>
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassName="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color={linkColour}></FontAwesomeIcon>
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color={linkColour} />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/ntjnh"
+        >
+          <FontAwesomeIcon icon={faGithub} color={linkColour} />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.freecodecamp.org/ntjnh"
+        >
+          <FontAwesomeIcon icon={faFreeCodeCamp} color={linkColour} />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://codepen.io/ntjnh"
+        >
+          <FontAwesomeIcon icon={faCodepen} color={linkColour} />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
