08da7614aac945a8bdd1146c6299db08ed57b508
Nate J.M <nate@natejonah.com>
2022-11-05 13:25:38 +0000

    Fix strokeWidth attr for svgs in Logo component

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -42,6 +42,7 @@ const Logo = () => {
       <svg
         width="972pt"
         height="897pt"
+        version="1.0"
         viewBox="0 0 728.01 671.5"
         xmlns="http://www.w3.org/2000/svg"
       >
@@ -50,14 +51,14 @@ const Logo = () => {
             ref={outlineLogoRef}
             fill="#fff"
             stroke="#0052bd"
-            stroke-width="2px"
+            strokeWidth="2px"
             d="M688.01,1V11.3l-87.55,51.5V670.5h-40.17L145.2,100.91V619l5.15,5.15h77.25v46.35H6.15v-46.35H83.4l5.15-5.15V62.8L1,11.3V1H175.07l368.74,507.79V62.8L456.26,11.3V1h231.75Z"
           />
           <path
             ref={outlineLogoRef}
             fill="#fff"
             stroke="#0052bd"
-            stroke-width="2px"
+            strokeWidth="2px"
             d="M727.01,1V11.3l-87.55,51.5V670.5h-40.17L184.2,100.91V619l5.15,5.15h77.25v46.35H45.15v-46.35H122.4l5.15-5.15V62.8L40,11.3V1H214.07l368.74,507.79V62.8L495.26,11.3V1h231.75Z"
           />
         </g>
